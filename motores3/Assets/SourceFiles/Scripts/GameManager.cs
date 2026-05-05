58a3fca12f3059bc2aec12cca92c15a272f1a057
Kaio Manoel Lima dos Santos <k.manoel@escolar.ifrn.edu.br>
2026-05-05 16:06:31 -0300

    finalizado

--- motores3/Assets/SourceFiles/Scripts/GameManager.cs
+++ motores3/Assets/SourceFiles/Scripts/GameManager.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager Instance;
+
+    public GameState currentState;
+
+    public PlayerInput playerInput;
+
+    private void Awake()
     {
-        
+        // Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SetState(GameState.Iniciando);
+        LoadScene("Splash");
+    }
+
+    public void SetState(GameState newState)
+    {
+        currentState = newState;
+        Debug.Log("Estado atual: " + currentState);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        switch (sceneName)
+        {
+            case "Splash":
+                SetState(GameState.Iniciando);
+                break;
+
+            case "MenuPrincipal":
+                SetState(GameState.MenuPrincipal);
+                break;
+
+            case "SampleScene":
+                SetState(GameState.Gameplay);
+                break;
+        }
+    }
+
+    public void AssignInputToPlayer()
+    {
+        if (playerInput != null)
+        {
+            playerInput.ActivateInput();
+            Debug.Log("Input ativado");
+        }
     }
 }
